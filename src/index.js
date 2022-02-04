6067a962375fe21c621ccff7e27a4441fa253838
vannesa <manelp3214@gmail.com>
2022-02-03 21:13:48 -0600

    Empezamos a general las imagnes con js

--- src/index.js
+++ src/index.js
@@ -3,4 +3,30 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+//crear imagen
+
+   /* <div class="p-4">
+    <img 
+    class="mx-auto" 
+    width="320" 
+    src="https://randomfox.ca/images/2.jpg" 
+    alt=""
+    />
+  </div>  */
+  const createImageNode= () => {
+    const container = document.createElement("div");
+    container.className = "p-4";
+
+      const imagen = document.createElement('img');
+      imagen.className = "mx-auto";
+      imagen.width = "320";
+      imagen.src = "https://randomfox.ca/images/2.jpg"; //TODO
+
+      container.appendChild(imagen);
+      return imagen;
+};
+
+const nuevaImagen = createImageNode();
+const mountNode = document.getElementById("images").appendChild(nuevaImagen);
+//agregar a contenedor imagen
+
